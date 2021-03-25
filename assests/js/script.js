dd197824e85cd01c8f49dc3bcc71d6312e1a03d2
DINNERman <shrwnwilliams@gmail.com>
2021-03-25 18:39:41 -0400

    struggled to figure out how to save textarea content, to no avail

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,10 +1,20 @@
 // DOM Variables
-var saveButton = $('#save-button>');
-var hourOfDayEl = $('#hour-of-day');
+var saveButton = $(".save-button>");
+var saveIcon = $(".saveIcon")
+var hourOfDayEl = $("#hour-of-day");
+var mainContainer = $(".container");
+var plannerContent = $(".plannerInput").val('');
+var planner9 = $("#9-input")
 
 // var businessHours = [ 9, 10, 11, 12, 1, 2, 3, 4 ,5]
 
-console.log(businessHours);
+// console.log(businessHours);
+
+// var savedPlannerContent = localStorage.getItem("plannerContent");
+
+// plannerContent.text(savedPlanneContent)
+
+// var userContent = planner9.val()
 
 var currentDayDisplay = $("#currentDay");
 var today = moment();
@@ -15,7 +25,15 @@ saveButton.on("click", function(){
     alert("good work");
 });
 
-for (var i = 0; i<businessHours.length; i++) {
-    hourOfDayEl.text(businessHours[i]);
+// for (var i = 0; i<businessHours.length; i++) {
+//     hourOfDayEl.text(businessHours[i]);
 
-}
+// }
+
+mainContainer.on("click", function(event) {
+    var saveTarget = event.target;
+
+    if (saveTarget.matches("button") || saveTarget.matches("i")) {
+        alert("You pushed a button");
+    };
+});
