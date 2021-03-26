b0c36f88e56b319b8a15d5ff67ad2dd78bbda5b2
DINNERman <shrwnwilliams@gmail.com>
2021-03-25 20:56:07 -0400

    discovered a way to potentially create the rows in js rather than hardcoding them into html

--- assests/js/script.js
+++ assests/js/script.js
@@ -16,25 +16,38 @@ var planner9 = $("#9-input")
 
 // var userContent = planner9.val()
 
+
 var currentDayDisplay = $("#currentDay");
 var today = moment();
 
 currentDayDisplay.text(today.format("dddd, MMMM Do, YYYY"));
-
-saveButton.on("click", function(){
-    alert("good work");
-});
-
 // for (var i = 0; i<businessHours.length; i++) {
 //     hourOfDayEl.text(businessHours[i]);
 
 // }
 
+
+
+function renderSavedContent (){
+    var content = localStorage.getItem("plannerContent");
+    plannerContent.text(content);
+}
+
 mainContainer.on("click", function(event) {
     var saveTarget = event.target;
 
     if (saveTarget.matches("button") || saveTarget.matches("i")) {
-        alert("You pushed a button");
+        var plannerGoods = plannerContent.val()
         console.log(plannerContent.val());
+        localStorage.setItem("plannerContent", plannerGoods);
     };
 });
+
+function init () {
+    renderSavedContent();
+};
+
+init();
+
+var txt2 = $("<p></p>").text("woah"); 
+$("body").append(txt2);
