a55ceb42af011dbb0bf12a472ecc9b40b14e1b18
DINNERman <shrwnwilliams@gmail.com>
2021-03-25 21:20:12 -0400

    added a conditional to potentially change the color of the text area based on the time

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,17 +1,14 @@
 // DOM Variables
 var saveButton = $(".save-button>");
-var saveIcon = $(".saveIcon")
+var saveIcon = $(".saveIcon");
 var hourOfDayEl = $("#hour-of-day");
 var mainContainer = $(".container");
 var plannerContent = $(".plannerInput");
-var planner9 = $("#9-input")
+var planner9 = $("#9-input");
 
 // var businessHours = [ 9, 10, 11, 12, 1, 2, 3, 4 ,5]
 
 // console.log(businessHours);
-
-
-
 
 var currentDayDisplay = $("#currentDay");
 var today = moment();
@@ -19,33 +16,42 @@ var today = moment();
 currentDayDisplay.text(today.format("dddd, MMMM Do, YYYY"));
 // for (var i = 0; i<businessHours.length; i++) {
 //     hourOfDayEl.text(businessHours[i]);
-
 // }
 
-
-
-function renderSavedContent (){
-    var content = localStorage.getItem("plannerContent");
-    plannerContent.text(content);
+function init() {
+  renderSavedContent();
+  changeTime();
 }
 
-mainContainer.on("click", function(event) {
-    var saveTarget = event.target;
+function changeTime () {
+    var currentTime = moment().format("h:mm:ss a")
+    if (currentTime < 8){
+        planner9.addClass("past");
+    } else if (currentTime > 9) {
+        planner9.addClass("future");
+    } else {
+        planner9.addClass("present");
+    }
 
-    if (saveTarget.matches("button") || saveTarget.matches("i")) {
-        var plannerGoods = plannerContent.val()
-        console.log(plannerContent.val());
-        localStorage.setItem("plannerContent", plannerGoods);
-    };
+}
+
+function renderSavedContent() {
+  var content = localStorage.getItem("plannerContent");
+  plannerContent.text(content);
+}
+
+mainContainer.on("click", function (event) {
+  var saveTarget = event.target;
+
+  if (saveTarget.matches("button") || saveTarget.matches("i")) {
+    var plannerGoods = plannerContent.val();
+    console.log(plannerContent.val());
+    localStorage.setItem("plannerContent", plannerGoods);
+  }
 });
 
-function init () {
-    renderSavedContent();
-};
 
 init();
 
-
-
-// var txt2 = $("<p></p>").text("woah"); 
+// var txt2 = $("<p></p>").text("woah");
 // $("body").append(txt2);
